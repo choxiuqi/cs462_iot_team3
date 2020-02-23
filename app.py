cf19938479bb9b4e57142d1b58874447f214157b
choxiuqi <46753932+choxiuqi@users.noreply.github.com>
2020-02-23 17:57:42 +0800

    edited app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.debug = True #to set in staging development
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sxwvjgwhgvzwcu://smt203team2:9c7e10ad14854f8266bb4830c93f9c412d621b945ad04bd82f73c28b44247423@ec2-18-235-20-228.compute-1.amazonaws.com:5432/d3tvcbdn77mrah'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+from models import PIRsensor, Record, MeetingRoom
