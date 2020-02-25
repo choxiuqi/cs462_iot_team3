b30ca918bde668b9fc587ca353941ba1e7b2df59
choxiuqi <46753932+choxiuqi@users.noreply.github.com>
2020-02-25 16:21:47 +0800

    Update models.py

--- models.py
+++ models.py
@@ -6,49 +6,28 @@ class PIRsensor(db.Model):
     
     id = db.Column(db.Integer, primary_key=True) 
     desc = db.Column(db.String(120), unique=False, nullable=False) 
-    meeting_room_id = db.Column(db.Integer, unique=False, nullable=False) 
+    meeting_room_id = db.Column(db.Integer, db.ForeignKey('meeting_room.id'), unique=False, nullable=False) 
 
     # one-to-many model
 
+    meeting_room = db.relationship('MeetingRoom', back_populates='sensors')
     records = db.relationship('Record', back_populates='sensor', cascade='all', lazy=True, uselist=True) 
-    meeting_room = db.relationship('MeetingRoom', back_populates='sensors', cascade='all', lazy=True)
 
 
-    def __init__(self, id, desc, meeting_room_id): 
+    def __init__(self, id, desc, meeting_room_id, records=None): 
         self.id = id 
         self.desc = desc 
         self.meeting_room_id = meeting_room_id
+        records = [] if records is None else records
+        self.records = records
+
     
     def serialize(self): 
         return { 
             'id': self.id, 
             'desc': self.desc, 
-            'meeting_room_id' : self.meeting_room_id, 
+            'meeting_room_id': self.meeting_room_id, 
             'records': [r.serialize() for r in self.records] 
-        }
-
-
-class Record(db.Model): 
-    __tablename__ = 'record' #database table name, optionally specified 
-    id = db.Column(db.Integer, primary_key=True) 
-    value = db.Column(db.Integer, nullable=False) 
-    timestamp = db.Column(db.DateTime, unique=False, nullable=False) 
-    sensor_id = db.Column(db.Integer, db.ForeignKey('pir_sensor.id'), unique=False, nullable=False, )
-    
-    # one-to-many model
-    sensor = db.relationship('PIRSensor', back_populates='records')
-
-    def __init__(self, value, timestamp, sensor_id): 
-        self.value = value
-        self.timestamp = timestamp
-        self.sensor_id = sensor_id
-
-    def serialize(self):
-        return {
-            'record_id': self.id,
-            'value': self.value,
-            'timestamp':self.timestamp,
-            'sensor_id':self.sensor_id
         }
 
 class MeetingRoom(db.Model): 
@@ -56,16 +35,43 @@ class MeetingRoom(db.Model):
     id = db.Column(db.Integer, primary_key=True) 
     current_occupancy = db.Column(db.Integer, unique=False, nullable=True) 
     
+    # one-to-many relationship
     sensors = db.relationship('PIRsensor', back_populates='meeting_room', lazy=True, uselist=True)
 
-    def __init__(self, id, sensors, current_occupancy=None): 
+    def __init__(self, id, sensors=None, current_occupancy=None): 
         self.id = id 
-        self.sensors = [] if sensors is None else sensors 
+        sensors = [] if sensors is None else sensors
         self.sensors = sensors
+        self.current_occupancy = current_occupancy
 
     def serialize(self):
         return {
             'meeting_room_id' : self.id,
-            'current_occupancy' : self.current_occupnacy,
-            'sensors' : self.sensors
+            'current_occupancy' : self.current_occupancy,
+            'sensors' : [s.serialize() for s in self.sensors]
+        }
+
+class Record(db.Model): 
+    __tablename__ = 'record' #database table name, optionally specified 
+    id = db.Column(db.Integer, primary_key=True) 
+    value = db.Column(db.Integer, nullable=False) 
+    timestamp = db.Column(db.DateTime, unique=False, nullable=False) 
+    sensor_id = db.Column(db.Integer, db.ForeignKey('pir_sensor.id'), unique=False, nullable=False)
+    
+    # one-to-many model
+    sensor = db.relationship('PIRSensor', back_populates='records', cascade='all', lazy=True)
+
+    def __init__(self, value, timestamp, sensor_id): 
+        self.value = value
+        self.timestamp = timestamp
+        self.sensor_id = sensor_id
+
+        # self.sensor = None
+
+    def serialize(self):
+        return {
+            'record_id': self.id,
+            'value': self.value,
+            'timestamp':self.timestamp,
+            'sensor_id':self.sensor_id
         }
