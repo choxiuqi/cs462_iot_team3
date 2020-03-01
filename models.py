9893fa347bb1918de3de66f12ad56c0edcea0785
choxiuqi <46753932+choxiuqi@users.noreply.github.com>
2020-03-01 18:09:23 +0800

    Update models.py

--- models.py
+++ models.py
@@ -2,7 +2,7 @@ from app import db
 import datetime 
 
 class Sensor(db.Model): 
-    __tablename__ = 'pir_sensor' #database table name, optionally specified 
+    __tablename__ = 'sensor' #database table name, optionally specified 
     
     id = db.Column(db.Integer, primary_key=True) 
     desc = db.Column(db.String(80), unique=False, nullable=False)
@@ -34,22 +34,26 @@ class Sensor(db.Model):
 class MeetingRoom(db.Model): 
     __tablename__ = 'meeting_room' 
     id = db.Column(db.Integer, primary_key=True) 
-    current_occupancy = db.Column(db.Integer, unique=False, nullable=True) 
-    
+    capacity = db.Column(db.Integer, unique=False, nullable=True)
+
     # one-to-many relationship
     sensors = db.relationship('Sensor', back_populates='meeting_room', lazy=True, uselist=True)
+    occupancy_records = db.relationship('Occupacy', back_populates='meeting_room', lazy=True, uselist=True)
 
-    def __init__(self, id, sensors=None, current_occupancy=None): 
+    def __init__(self, id, capacity, sensors=None, occupancy_records=None): 
         self.id = id 
+        self.capacity = capacity
         sensors = [] if sensors is None else sensors
         self.sensors = sensors
-        self.current_occupancy = current_occupancy
+        occupancy_records = [] if occupancy_records is None else occupancy_records
+        self.occupancy_records = occupancy_records
 
     def serialize(self):
         return {
             'meeting_room_id' : self.id,
-            'current_occupancy' : self.current_occupancy,
-            'sensors' : [s.serialize() for s in self.sensors]
+            'capacity' : self.capacity,
+            'sensors' : [s.serialize() for s in self.sensors],
+            'occupancy_records' : [o.serialize() for o in self.occupancy_records]
         }
 
 class Record(db.Model): 
@@ -57,7 +61,7 @@ class Record(db.Model):
     id = db.Column(db.Integer, primary_key=True) 
     value = db.Column(db.Integer, nullable=False) 
     timestamp = db.Column(db.DateTime, unique=False, nullable=False) 
-    sensor_id = db.Column(db.Integer, db.ForeignKey('pir_sensor.id'), unique=False, nullable=False)
+    sensor_id = db.Column(db.Integer, db.ForeignKey('sensor.id'), unique=False, nullable=False)
     
     # one-to-many model
     sensor = db.relationship('Sensor', back_populates='records', cascade='all', lazy=True)
@@ -75,4 +79,28 @@ class Record(db.Model):
             'value': self.value,
             'timestamp':self.timestamp,
             'sensor_id':self.sensor_id
+        }
+
+class Occupacy(db.Model): 
+    __tablename__ = 'occupancy' 
+    id = db.Column(db.Integer, primary_key=True)
+    timestamp = db.Column(db.DateTime, unique=False) 
+    meeting_room_id = db.Column(db.Integer, db.ForeignKey('meeting_room.id'), unique=False, nullable=False)
+    value = db.Column(db.Integer, unique=False, nullable=True)
+
+    # one-to-many relationship
+    meeting_room = db.relationship('MeetingRoom', back_populates='occupancy_records')
+
+    def __init__(self, id, timestamp, meeting_room_id, value): 
+        self.id = id 
+        self.timestamp = timestamp
+        self.meeting_room_id = meeting_room_id
+        self.value = value
+
+    def serialize(self):
+        return {
+            'id' : self.id,
+            'timestamp' : self.timestamp,
+            'meeting_room_id': self.meeting_room_id,
+            'value' : self.value
         }
