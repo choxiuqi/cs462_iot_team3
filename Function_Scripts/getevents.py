4fb66fd711f67533402b424a898df61f97599f95
rielleriri <41384988+rielleriri@users.noreply.github.com>
2020-03-13 21:04:12 +0800

    Update getevents.py

--- Function_Scripts/getevents.py
+++ Function_Scripts/getevents.py
@@ -61,7 +61,7 @@ while True:
         creator = results[counter]['creator']
         start = results[counter]['start']
         end = results[counter]['end']
-        cur.execute("INSERT INTO upcoming VALUES (DEFAULT, %s, %s, %s);",(creator, start, end))
+        cur.execute("INSERT INTO upcoming VALUES(DEFAULT, %s, %s, %s);", (creator,start,end))
         counter += 1
     time.sleep(900)
     cur.execute("DELETE FROM upcoming;")
