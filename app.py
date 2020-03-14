23b7de714f7d6a6b69ffd49e340dc8f3d117f82f
kummylently <49142767+kummylently@users.noreply.github.com>
2020-03-14 16:58:14 +0800

    Update app.py

--- app.py
+++ app.py
@@ -49,6 +49,6 @@ def get_occupancy():
 @app.route('/latest_uss_record', methods=['GET']) 
 def get_latest_uss_record(): 
     latest_records = LatestUSSRecord.query.all()
-    return jsonify([s.serialize() for l in latest_records])
+    return jsonify([l.serialize() for l in latest_records])
 #to get the current reading occupancy in database 
 
