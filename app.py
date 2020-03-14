7208f98afdf8e308b9aa0fbd2557e8c40820a0b9
choxiuqi <46753932+choxiuqi@users.noreply.github.com>
2020-03-14 11:48:55 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://team3user:password@localho
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-from models import Sensor, MeetingRoom, USSRecord, LatestUSSRecord, Occupancy, SensorHealth, PIRRecord
+from models import Sensor, MeetingRoom, USSRecord, LatestUSSRecord, Occupancy, SensorHealth, PIRRecord, Upcoming
 
 @app.route('/hello/', methods=['GET']) 
 def hello():
