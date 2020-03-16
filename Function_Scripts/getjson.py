d8432876d0f574c4e71ecf3c4fca719d253a8ec0
allysonlimmm <40385546+allysonlimmm@users.noreply.github.com>
2020-03-16 15:59:54 +0800

    Update getjson.py

--- Function_Scripts/getjson.py
+++ Function_Scripts/getjson.py
@@ -21,6 +21,35 @@ def getevents(url):
         json.dump(events, f2)
     return ('events.json')
 
+
+import logging
+import boto3
+from botocore.exceptions import ClientError
+
+
+def upload_file(file_name, bucket, object_name=None):
+    """Upload a file to an S3 bucket
+
+    :param file_name: File to upload
+    :param bucket: Bucket to upload to
+    :param object_name: S3 object name. If not specified then file_name is used
+    :return: True if file was uploaded, else False
+    """
+
+    # If S3 object_name was not specified, use file_name
+    if object_name is None:
+        object_name = file_name
+
+    # Upload the file
+    s3_client = boto3.client('s3')
+    try:
+        response = s3_client.upload_file(file_name, bucket, object_name)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
+
+
 def s3(file):
     cmd = 'aws s3 cp {} s3://cs462g3'.format(file)
     os.system(cmd)
@@ -29,15 +58,15 @@ def main():
     baseURL = 'http://52.87.236.66:5000'
     meetingRoom = baseURL + '/occupancy'
     print("meeting room called")
-    s3(getoccupancy(meetingRoom))
+    upload_file(getoccupancy(meetingRoom), cs462g3)
     print("uploaded on s3")
     # sensorHealth = baseURL + '/sensor-health'
     events = baseURL + '/event'
     print("events called")
     # s3(getsensorhealth(sensorHealth))
-    s3(getevents(events))
+    upload_file(getevents(events), cs462g3)
     print("uploaded on s3")
 
 while True:
     main()
-    time.sleep(5)
+    time.sleep(300)
