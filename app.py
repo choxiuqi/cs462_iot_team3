ecb57fc4eeb7109cd9f38e899627e14d912954af
ingsin <46809547+ingsin@users.noreply.github.com>
2020-03-16 20:23:25 +0800

    Update app.py

--- app.py
+++ app.py
@@ -50,9 +50,8 @@ def sensorHealth():
                 for a in v:
                     a["desc"] = d1["desc"]
                     a["meeting_room_id"] = d1["meeting_room_id"]
-        return jsonify(final_dict)
-    #     final_list.append(final_dict)
-    # return jsonify(final_list)
+        final_list.append(final_dict)
+    return jsonify(final_list)
 
 @app.route('/occupancy', methods=['GET']) 
 def get_occupancy(): 
