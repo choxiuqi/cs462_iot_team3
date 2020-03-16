2c827b2e8530b4b7919a0214548d3ff8b45066fb
ingsin <46809547+ingsin@users.noreply.github.com>
2020-03-16 16:13:51 +0800

    Update app.py

--- app.py
+++ app.py
@@ -38,27 +38,19 @@ def get_sensors():
 @app.route('/sensor-health', methods=['GET'])
 def sensorHealth():
     health = Sensor.query.all()
-    # a1 = jsonify([h.health() for h in health])
-    # s1 = json.dumps(a1)
-    # json_data = json.loads(j1)
-    final_dict = {}
-    for temp_dict in health:
-        d1 = temp_dict.serialize()
-        final_dict["id"] = d1["id"]
-        final_dict["pir_records"] = d1["pir_records"]
-        final_dict["sensor_health"] = d1["sensor_health"]
-        for k,v in d1.items():
-            if k == "sensor_health":
-                for a in v:
-                    a["desc"] = d1["desc"]
-                    a["meeting_room_id"] = d1["meeting_room_id"]
-            # if k == "desc" or k == "meeting_room_id":
-            #     final_dict["sensor_health"] = {k:v}
-            # elif k == "sensor_health":
-            #     final_dict["sensor_health"] = v
-            # elif k != "desc" or k != "meeting_room_id":
-            #     final_dict[k] = v
-    return jsonify(final_dict)
+    # final_dict = {}
+    # for temp_dict in health:
+    #     d1 = temp_dict.serialize()
+    #     final_dict["id"] = d1["id"]
+    #     final_dict["pir_records"] = d1["pir_records"]
+    #     final_dict["sensor_health"] = d1["sensor_health"]
+    #     for k,v in d1.items():
+    #         if k == "sensor_health":
+    #             for a in v:
+    #                 a["desc"] = d1["desc"]
+    #                 a["meeting_room_id"] = d1["meeting_room_id"]
+    # return jsonify(final_dict)
+    return jsonify([h.serialize() for h in health])
 
 @app.route('/occupancy', methods=['GET']) 
 def get_occupancy(): 
