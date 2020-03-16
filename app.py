786921417fb5d5d002c0447f054f813776421ceb
ingsin <46809547+ingsin@users.noreply.github.com>
2020-03-16 17:41:14 +0800

    Update app.py

--- app.py
+++ app.py
@@ -41,17 +41,15 @@ def sensorHealth():
     final_dict = {}
     for temp_dict in health:
         d1 = temp_dict.serialize()
-        return(jsonify(d1))
-        
-    #     final_dict["id"] = d1["id"]
-    #     final_dict["pir_records"] = d1["pir_records"]
-    #     final_dict["sensor_health"] = d1["sensor_health"]
-    #     for k,v in d1.items():
-    #         if k == "sensor_health":
-    #             for a in v:
-    #                 a["desc"] = d1["desc"]
-    #                 a["meeting_room_id"] = d1["meeting_room_id"]
-    # return jsonify(final_dict)
+        final_dict["id"] = d1["id"]
+        final_dict["pir_records"] = d1["pir_records"]
+        final_dict["sensor_health"] = d1["sensor_health"]
+        # for k,v in d1.items():
+        #     if k == "sensor_health":
+        #         for a in v:
+        #             a["desc"] = d1["desc"]
+        #             a["meeting_room_id"] = d1["meeting_room_id"]
+    return jsonify(final_dict)
 
 @app.route('/occupancy', methods=['GET']) 
 def get_occupancy(): 
