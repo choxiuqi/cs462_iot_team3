257894718cab3f94ef0076be4ec07048263fd6b3
allysonlimmm <40385546+allysonlimmm@users.noreply.github.com>
2020-03-16 21:15:14 +0800

    Update getjson.py

--- Function_Scripts/getjson.py
+++ Function_Scripts/getjson.py
@@ -24,16 +24,19 @@ def tocsv(msg):
 
 def getoccupancy(url):
     occupancy = requests.get(url).json()[0]
-    with open('occupancy.json', 'w') as f1:
-        json.dump(occupancy, f1)
-        # for key in occupancy.keys():
-        #     f1.write("%s, %s\n"%(key, occupancy[key]))
+    dictionary = json.loads(occupancy)
+
+    # with open('occupancy.json', 'w') as f1:
+    #     json.dump(occupancy, f1)
+    with open('occupancy.csv', 'w') as f1
+        for key in dictionary.keys():
+            f1.write("%s, %s\n"%(key, dictionary[key]))
         # f1.write("id", "meeting_room_id", "timestamp", "value", '\n')
         # for i in occupancy:
         #     for k,v in i.items():
         #         f1.write(v)
         # json.dump(occupancy, f1)
-    return ('occupancy')
+    return ('occupancy.csv')
 
 def getsensorhealth(url):
     sensors = requests.get(url).json()[0]['sensor_health']
@@ -65,7 +68,7 @@ def main():
     baseURL = 'http://3.80.134.50:5000'
     meetingRoom = baseURL + '/occupancy'
     print("meeting room called")
-    s3(tocsv(getoccupancy(meetingRoom)))
+    s3(getoccupancy(meetingRoom))
     print("uploaded on s3")
     # sensorHealth = baseURL + '/sensor-health'
     # print("sensor health called")
