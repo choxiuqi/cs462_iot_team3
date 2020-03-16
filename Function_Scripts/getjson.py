abb190715d7d6ce8b7053e3d8f92303fb9a15b72
allysonlimmm <40385546+allysonlimmm@users.noreply.github.com>
2020-03-16 21:38:23 +0800

    Update getjson.py

--- Function_Scripts/getjson.py
+++ Function_Scripts/getjson.py
@@ -26,36 +26,29 @@ def getoccupancy(url):
     occupancy = requests.get(url).json()[0]
     occ_dict = json.dumps(occupancy)
     dictionary = json.loads(occ_dict)
-
-
-    # with open('occupancy.json', 'w') as f1:
-    #     json.dump(occupancy, f1)
-
     with open('occupancy.csv', 'w') as f1:
         for key in dictionary.keys():
             f1.write("%s, %s\n"%(key, dictionary[key]))
-
-        # f1.write("id", "meeting_room_id", "timestamp", "value", '\n')
-        # for i in occupancy:
-        #     for k,v in i.items():
-        #         f1.write(v)
-        # json.dump(occupancy, f1)
-    # print(dictionary)
     return ('occupancy.csv') 
 
 def getsensorhealth(url):
-    sensors = requests.get(url).json()[0]['sensor_health']
+    for i in range(0, 4):
+        sensor = requests.get(url).json()[i]['sensor_health']
+        sensor_dict = json.dumps(sensor)
+        sensors = json.loads(sensor_dict)
     # with open('sensors.json', 'w') as f2:
     #     json.dump(uss_in, f2)
-    with open('sensors.csv', 'w') as f2:
-        for key in sensors.keys():
-            f1.write("%s, %s\n"%(key, sensors[key]))
+        with open('sensors.csv', 'a') as f2:
+            for key in sensors.keys():
+                f1.write("%s, %s\n"%(key, sensors[key]))
     return ('sensors.csv')
 
 def getevents(url):
-    events = requests.get(url).json()
+    event = requests.get(url).json()
     # with open('events.json', 'w') as f3:
     #     json.dump(events, f3)
+    event_dict = json.dumps(event)
+    events = json.loads(event_dict)
     with open('events.csv', 'w') as f3:
         for i in events:
             for key in i.keys():
@@ -76,14 +69,14 @@ def main():
     getoccupancy(meetingRoom)
     s3(getoccupancy(meetingRoom))
     print("uploaded on s3")
-    # sensorHealth = baseURL + '/sensor-health'
-    # print("sensor health called")
-    # s3(getsensorhealth(sensorHealth))
-    # print("uploaded on s3")
-    # events = baseURL + '/event'
-    # print("events called")
-    # s3(getevents(events))
-    # print("uploaded on s3")
+    sensorHealth = baseURL + '/sensor-health'
+    print("sensor health called")
+    s3(getsensorhealth(sensorHealth))
+    print("uploaded on s3")
+    events = baseURL + '/event'
+    print("events called")
+    s3(getevents(events))
+    print("uploaded on s3")
 
 while True:
     main()
