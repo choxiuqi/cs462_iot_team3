0729306469141995695059ef3108e8732f96e12d
allysonlimmm <40385546+allysonlimmm@users.noreply.github.com>
2020-03-16 20:32:56 +0800

    Update getjson.py

--- Function_Scripts/getjson.py
+++ Function_Scripts/getjson.py
@@ -22,8 +22,8 @@ def getsensorhealth(url):
     # with open('sensors.json', 'w') as f2:
     #     json.dump(uss_in, f2)
     with open('sensors.csv', 'w') as f2:
-        for i in sensors:
-            f2.write(i)
+        for key in sensors.keys():
+            f1.write("%s, %s\n"%(key, sensors[key]))
     return ('sensors.csv')
 
 def getevents(url):
@@ -32,11 +32,14 @@ def getevents(url):
     #     json.dump(events, f3)
     with open('events.csv', 'w') as f3:
         for i in events:
-            f3.write(i)
+            for key in i.keys():
+                f3.write("%s, %s\n"%(key, eveints[key]))
+        # for i in events:
+        #     f3.write(i)
     return ('events.csv')
 
 def s3(file):
-    cmd = 'aws s3 cp {} s3://cs462g3'.format(file)
+    cmd = 'aws s3 cp {} s3://cs462g3/data'.format(file)
     os.system(cmd)
     return
 
@@ -46,12 +49,14 @@ def main():
     print("meeting room called")
     s3(getoccupancy(meetingRoom))
     print("uploaded on s3")
-    # sensorHealth = baseURL + '/sensor-health'
-    # s3(getsensorhealth(sensorHealth))
-    # events = baseURL + '/event'
-    # print("events called")
-    # s3(getevents(events))
-    # print("uploaded on s3")
+    sensorHealth = baseURL + '/sensor-health'
+    print("sensor health called")
+    s3(getsensorhealth(sensorHealth))
+    print("uploaded on s3")
+    events = baseURL + '/event'
+    print("events called")
+    s3(getevents(events))
+    print("uploaded on s3")
 
 while True:
     main()
