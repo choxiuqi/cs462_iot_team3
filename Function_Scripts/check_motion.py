2eb26785b4822019953a30c6f7a96b5921194d92
choxiuqi <46753932+choxiuqi@users.noreply.github.com>
2020-03-18 00:23:18 +0800

    Update check_motion.py

--- Function_Scripts/check_motion.py
+++ Function_Scripts/check_motion.py
@@ -12,8 +12,8 @@ cur = conn.cursor()
 conn.autocommit = True
 
 def check_reset():
-    print('function called...')
-    cur.execute('SELECT "value", "timestamp" FROM pir_record_debug ORDER BY id DESC LIMIT 3;')
+    # print('function called...')
+    cur.execute('SELECT "value", "timestamp" FROM pir_record ORDER BY id DESC LIMIT 3;')
     last_three_readings = cur.fetchall()    
     occupied_or_not = 0
 
@@ -22,31 +22,35 @@ def check_reset():
         value = reading[0]
         occupied_or_not += value
     
-    print('final reading is: ', occupied_or_not,)
+    # print('final reading is: ', occupied_or_not,)
 
     #when there is a movement
     if occupied_or_not >0:
-        print("reset function done\n\n")
+        # print("reset function done\n\n")
         return 
 
     #when there is no movement (occipied_or_not==1)
     else:
         # get current occupancy
-        cur.execute('SELECT "value" FROM occupancy_debug ORDER BY id DESC LIMIT 1;')
-        last_occupancy = cur.fetchone()
-        print("last_occupancy is: ",last_occupancy)
+        cur.execute('SELECT "value" FROM occupancy ORDER BY id DESC LIMIT 1;')
+        last_occupancy = cur.fetchone()[0]
+        # print("last_occupancy is:",last_occupancy)
 
+        if last_occupancy > 0:
         # if current occupancy > 0
-        timestamp = str(datetime.now())[0:19]        
-        meeting_room_id = 'G'
-        new_occupancy = 0
-        remarks = "resetted"
-        cur.execute("INSERT INTO occupancy_debug VALUES (DEFAULT, %s, %s, %s, %s);",(timestamp, meeting_room_id, new_occupancy, remarks))
-        print('inserted reset into occupancy table \n\n')
+            timestamp = str(datetime.now())[0:19]        
+            meeting_room_id = 'G'
+            new_occupancy = 0
+            remarks = "resetted"
+            cur.execute("INSERT INTO occupancy VALUES (DEFAULT, %s, %s, %s, %s);",(timestamp, meeting_room_id, new_occupancy, remarks))
+            # print('inserted reset into occupancy table \n\n')
+            # return
 
+        return
+        # else:
         # if current occupancy = 0, do nothing
-        print("current occupancy is already 0")
-        return 
+            # print("current occupancy is already 0")
+            # return 
 
 
 try:
