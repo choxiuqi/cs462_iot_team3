fd85103efbf8c3d8ae2fd33b26022f27bc411e5e
choxiuqi <46753932+choxiuqi@users.noreply.github.com>
2020-03-17 20:35:46 +0800

    Update updateOccupancy_DEBUG.py

--- Function_Scripts/updateOccupancy_DEBUG.py
+++ Function_Scripts/updateOccupancy_DEBUG.py
@@ -38,14 +38,14 @@ def resetCounter():
     #     return 
     # #when there is no movement (occipied_or_not==1)
     # else:
-        #post ocupancy 1 new row to make occupancy 0,
-        time = last_five_readings[0][1]
-            # print("line 42- time is: ",time)        
-        meeting_room_id = 'G'
-        new_occupancy = 0
-        remarks = "resetted"
-        cur.execute("INSERT INTO occupancy_debug VALUES (DEFAULT, %s, %s, %s, %s);",(time, meeting_room_id, new_occupancy, remarks))
-        print("inserted and reset done")
+        # #post ocupancy 1 new row to make occupancy 0,
+        # time = last_five_readings[0][1]
+        #     # print("line 42- time is: ",time)        
+        # meeting_room_id = 'G'
+        # new_occupancy = 0
+        # remarks = "resetted"
+        # cur.execute("INSERT INTO occupancy_debug VALUES (DEFAULT, %s, %s, %s, %s);",(time, meeting_room_id, new_occupancy, remarks))
+        # print("inserted and reset done")
     return 
 
 # resetCounter()
