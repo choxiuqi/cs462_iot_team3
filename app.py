f25cf695780024bc3e1fa252f9ced5b3906a8747
allysonlimmm <40385546+allysonlimmm@users.noreply.github.com>
2020-03-18 09:39:48 +0800

    Update app.py

--- app.py
+++ app.py
@@ -64,23 +64,23 @@ def sensorHealthDebug():
     health = Sensor.query.all()
 
     final_list = []
-        for temp_dict in health:
-            final_dict = {}
-            d1 = temp_dict.serialize()
-            final_dict["id"] = d1["id"]
-            final_dict["pir_records"] = d1["pir_records"]
-            final_dict["sensor_health"] = d1["sensor_health"]
-            for k,v in d1.items():
-                if k == "sensor_health":
-                    for a in v:
-                        a["desc"] = d1["desc"]
-                        a["meeting_room_id"] = d1["meeting_room_id"]
-                elif k == "pir_records":
-                    for a in v:
-                        a["desc"] = d1["desc"]
-                        a["meeting_room_id"] = d1["meeting_room_id"]
-            final_list.append(final_dict)
-            
+    for temp_dict in health:
+        final_dict = {}
+        d1 = temp_dict.serialize()
+        final_dict["id"] = d1["id"]
+        final_dict["pir_records"] = d1["pir_records"]
+        final_dict["sensor_health"] = d1["sensor_health"]
+        for k,v in d1.items():
+            if k == "sensor_health":
+                for a in v:
+                    a["desc"] = d1["desc"]
+                    a["meeting_room_id"] = d1["meeting_room_id"]
+            elif k == "pir_records":
+                for a in v:
+                    a["desc"] = d1["desc"]
+                    a["meeting_room_id"] = d1["meeting_room_id"]
+        final_list.append(final_dict)
+
     return jsonify(final_list)
 
 @app.route('/occupancy', methods=['GET']) 
