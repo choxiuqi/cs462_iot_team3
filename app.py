1bf04386738370fb6b5c5cbd382b51e0b45406ee
allysonlimmm <40385546+allysonlimmm@users.noreply.github.com>
2020-04-04 11:32:25 +0800

    Update app.py

--- app.py
+++ app.py
@@ -103,7 +103,8 @@ def get_occupancy_debug():
         for k,v in d1.items():
             if k == 'timestamp':
                 # finalDict['timestamp'] = v.strftime('%d-%m-%Y %H:%M:%S')
-                finalDict['timestamp'] = v.strftime('%Y-%m-%d %H:%M:%S.%fZ')
+                finalDict['date'] = v.strftime('%Y-%m-%d')
+                finalDict['time'] = v.strftime('%H:%M:%S.%fZ')
         finalDict['value'] = d1['value']
         finalList.append(finalDict)
 
